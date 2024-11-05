0fe0738da331d75d7a776a65af45ec44ca15f301
LuisEgui <misionpage@gmail.com>
2024-11-05 19:25:57 +0100

    feat: update test_connection script to clean, compile, and run server with clients

--- p2/test_connection.sh
+++ p2/test_connection.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# 20 clients
-for i in {1..100}
-do
-    ./client http://localhost:22500 10 + 10 &
+# Clean previous files
+make clean
+
+# Compile saop library and make
+soapcpp2 -b -c conecta4.h
+make
+
+# Start the server and redirect its output to a log file
+./server 22500
+
+# Start the clients and redirect their output to log files
+for (( i=1; i<=4; i++ )); do
+    echo "Player $i" | ./client http://localhost:22501 > "client_$i.log" 2>&1 &
 done
